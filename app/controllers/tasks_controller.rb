8b2743ee7cac6daae3e2061aa4b36083beb7a20d
Travis Cashion <58757351+TCashion@users.noreply.github.com>
2024-03-20 11:10:53 -0600

    configure 'show' view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,11 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
   def new
     @task = Task.new
   end
