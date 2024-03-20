bf9e5d567540162d9f416a3d5f47fb108b1f2dea
Travis Cashion <58757351+TCashion@users.noreply.github.com>
2024-03-20 11:07:51 -0600

    define resource and create 'new' view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # Define the root path route ("/") to the tasks#index controller action so that home route shows a list of tasks.
   root "tasks#index"
+
+  # Define the tasks resource routes to create a full set of routes for the tasks resource.
+  resources :tasks
 end
